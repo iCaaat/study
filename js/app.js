ef80bf66011f600fa2c3f20931dc8848917d64cb
Gooood <2696805297@qq.com>
2024-09-13 10:33:34 +0800

    分文件

---
+++ js/app.js
@@ -0,0 +1,27 @@
+const images = [
+    { src: 'img/月.jpg', title: '月', description: '好看吧，我拍的！' },
+    { src: 'img/公园的夫妇.jpg', title: '公园的夫妇', description: '拍摄于衡阳平湖公园，感觉这一幕很暖心。' },
+    { src: 'img/衡山.jpg', title: '敢问路在何方', description: '拍摄于衡山山顶' }
+  ];
+
+  let currentIndex = 0;
+
+  function updateContent(index) {
+    const imageElement = document.getElementById('image');
+    const titleElement = document.getElementById('title');
+    const descriptionElement = document.getElementById('description');
+
+    imageElement.src = images[index].src;
+    titleElement.textContent = images[index].title;
+    descriptionElement.textContent = images[index].description;
+  }
+
+  function nextImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    updateContent(currentIndex);
+  }
+
+  function prevImage() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    updateContent(currentIndex);
+  }
